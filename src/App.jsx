0f0301923b507c84e65ab5c9ac859daf94b373b5
Jonhycristian <wketapi@gmail.com>
2026-06-19 09:30:31 -0300

    style: remover sombra da logo no aplicativo

--- src/App.jsx
+++ src/App.jsx
@@ -597,7 +597,7 @@ export default function App() {
                 <img src="/app.png" alt="Aplicativo CONTEC" className="w-full h-auto object-cover rounded-2xl" />
                 
                 {/* Logo sobreposta */}
-                <div className="absolute top-6 md:top-8 mt-[2px] left-1/2 -translate-x-1/2 pointer-events-none drop-shadow-[0_10px_20px_rgba(0,0,0,0.5)]">
+                <div className="absolute top-6 md:top-8 mt-[2px] left-1/2 -translate-x-1/2 pointer-events-none">
                   <img src={logo1} alt="Logo CONTEC" className="w-20 h-auto opacity-90" />
                 </div>
               </div>
